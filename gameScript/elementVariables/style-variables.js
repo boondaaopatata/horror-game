619e3033841de38b3696d15692b15e2f587938f2
SAMO <149397431+boondaaopatata@users.noreply.github.com>
2026-05-05 14:13:20 +0400

    Delete gameScript directory

--- gameScript/elementVariables/style-variables.js
+++
@@ -1,9 +0,0 @@
-const selectedEffect = "selected";
-const treeChoped = "tree-choped";
-
-const styleVariables = {
-    selectedEffect: selectedEffect,
-    treeChoped: treeChoped,
-}
-
-export default styleVariables;
--- gameScript/index.js
+++
@@ -1,8 +0,0 @@
-import generatePlayer from "./generate/generate-components/generate-character.js";
-import allMovementFunctionality from "./movement.js";
-import startRain from "./generate/generate-components/generate-rain.js";
-import gameImprovementsFunctions from "./game-improvements-functions.js";
-
-//gameImprovementsFunctions.removeContextMenu();
-generatePlayer();
-allMovementFunctionality();
